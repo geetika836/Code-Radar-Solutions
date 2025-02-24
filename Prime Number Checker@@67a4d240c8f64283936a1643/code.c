832007c30632d767710d8a457ce206b5073e976e
geetika836 <geetika1156.becse24@chitkara.edu.in>
2025-02-24 18:25:17 +0530

    Solution to Prime Number Checker

---
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -0,0 +1,15 @@
+// Your code here...
+int isPrime(int n)
+{
+    int arr[n];
+    while(n--)
+    {
+        int numbers;
+        scanf("%d",&numbers);
+        arr.add(numbers);
+    }
+    for i in arr
+    {
+
+    }
+}
