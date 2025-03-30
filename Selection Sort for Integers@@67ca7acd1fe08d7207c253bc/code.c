668ef2b7a7cd5672f1dcf84c4e428c7d3bfbc4e4
geetika836 <geetika1156.becse24@chitkara.edu.in>
2025-03-30 18:08:08 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -1,11 +1,11 @@
 // Your code here...
 #include<stdio.h>
-int swap(int arr[mini],int arr[i]){
+void swap(int arr[mini],int arr[i]){
     int temp=arr[mini];
     int arr[mini]=arr[i];
     int arr[i]=temp;
 }
-int selectionSort (int &arr,int &n){
+void selectionSort (int &arr,int &n){
     for(int i=0,i<=n-2;i++){
         int mini=i;
         for(int j=i;j<=n-1;j++){
