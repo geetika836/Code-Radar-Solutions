614cd2f25db3319f68daae2fc4b8ba23a360076f
geetika836 <geetika1156.becse24@chitkara.edu.in>
2025-03-30 19:08:42 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -6,7 +6,7 @@ void bubbleSort(int arr[],int n){
             if(arr[j]>arr[j+1]){
                 int temp=arr[j];
                 arr[j]=arr[j+1];
-                arr[j+1]=arr[j];
+                arr[j+1]=temp;
             }
         }
     }
